6d9bc99bad36f323e6cebcdfc36cfc01ac902c2c
Vikas Burman <vikasburman@gmail.com>
2019-08-08 01:16:31 -0400

    ...

--- src/www/features/myapp.feature1/myapp.feature1.views/Error404View.js
+++ src/www/features/myapp.feature1/myapp.feature1.views/Error404View.js
@@ -8,7 +8,9 @@ $$('ns', '(auto)');
 Class('(auto)', VueView, function() {
     this.title = "Not Found";
     this.html = `
-        <div><h2>{{ i18n('strings', 'notfound', 'Not Found') )}}</h2></div>
+        <div>
+            <h2>{{ i18n('strings', 'notfound', 'Not Found') }}</h2>
+        </div>
     `;
     this.i18n = {
         titles: "./titles.json",
